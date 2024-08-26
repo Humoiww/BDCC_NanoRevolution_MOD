d05de8912db585964d7b1aef432a0b806bf3b2c4
Humoiww <humoi020312@gmail.com>
2024-08-26 23:48:35 +0800

    update

--- Scenes/NanoSetting.gd
+++ Scenes/NanoSetting.gd
@@ -22,7 +22,7 @@ func _init():
 	var basicSpeciesDict = []
 	for speciesID in allSpecies:
 		var specie = allSpecies[speciesID]
-		if(!specie.canBeUsedForNPCType(speciesType)):
+		if(!specie.canBeUsedForNPCType("guard")):
 			continue
 		
 		var weight = GM.main.getEncounterSettings().getSpeciesWeight(speciesID)
@@ -33,26 +33,135 @@ func _init():
 		nanoSpeciesDict = basicSpeciesDict
 		setModuleFlag("NanoRevolutionModule", "NanoAndroidSpeciesDistr",nanoSpeciesDict)
 
+
 func _run():
 	if(state == ""):
-		addCharacter("humoi")
-		playAnimation(StageScene.Duo, "stand", {pc = "pc", npc="humoi"})
-		saynn("Out the corner of your eye, a glint of light catches your attention. You look around for its source, and spot a lilac inmate wearing a pair of old-fashioned glasses that must have reflected the ceiling lights. Who wears glasses anymore? He's talking to another inmate about something.")
+		saynn("You look around, a lilac inmate with a strangle datapad walks toward you.")
 
-		saynn("[say=vion]...and if you want a refresher, do not hesitate to visit. I spend as much time as possible in my cell these days anyway, and I won't mind the company.[/say]")
-		
-		saynn("True to his word, now that his business is concluded he makes haste in the direction of the cellblocks.")
+		saynn("[say=humoi]Ah, a new face. Let me check… inmate number {pc.inmateNumber}, {pc.name} right? [/say]")
 
-		saynn("[say=pc]I wonder what that was all about?[/say]")
-		
-		saynn("Well, you can always find out later. By the sounds of it, you can find this dragon in the lilac cellblock.")
-		
-		addButton("Leave", "No need to rush", "endthescene")
+		saynn("She is holding her datapad, seemingly typing something.")
+
+		saynn("[say=pc]Yes, that's me. Uh.. you are an inmate right?[/say]")
+
+		saynn("[say=humoi]100% natural lilac inmate~ My name is humoi, good to see you. About this datapad? I have a part-time job from the medical department to check inmate psychological status.[/say]")
+
+		saynn("[say=pc]Psychological status?[/say]")
+
+		saynn("[say=humoi]Well, several months ago, BDCC started the android assistant program, a program that lets these androids assist some tough tasks, like inspecting inmates. It works at first, but progressively, multiple cases indicate that these androids, especially their punishment mechanic, may cause some mental problems. [/say]")
+
+		saynn("She sighs a little, and continues.")
+
+		saynn("[say=humoi]Though I really like their size and punishment, I don't want others to feel uncomfortable with it. So I asked Eliza, the doctor in charge of the medical team here, to address this issue by using questionnaires. We will collect inmates' responses periodically, report their requirements to the engineering department and apply the changes.[/say]")
+
+		saynn("She's getting closer.")
+
+		saynn("[say=humoi]Now, do you have time to fill this little questionnaire for me? You can use me after you answering whole question~ [/say]")
+
+		saynn("Hum, maybe that's why she suggested a questionnaire solution to the medic.")
+
+		addButton("Certainly","I can do that. (Note: this questionnaire actually is an in-game setting scene for you to adjust the nano android spawning rule.)","Start_survey")
+
+	if(state == "Start_survey"):
+		saynn("She smiles, passing the datapad to you.")
+
+		saynn("[say=humoi]Excellent, here you are. Just listen your heart and answer these questions.[/say]")
+
+		addButton("Datapad","","Enter_survey")
+
+	if(state == "Enter_survey"):
+		saynn("=============================================")
+
+		saynn("Nano Android Feedback - Inmate Version")
+
+		saynn("=============================================")
+
+		saynn("Welcome, this survey is intended to listen to all inmates' advice about the nano android program.")
+
+		saynn("- [agree] I acknowledge my records will be used for adjusting the android generating rule and evaluating the prison safety index.")
+
+		saynn("- [decline] Exit the survey.")
+
+		addButton("agree","go to question 1","Q1")
+
+	if(state == "Q1"):
+		saynn("=============================================")
+
+		saynn("Nano Android Feedback - Inmate Version")
+
+		saynn("=============================================")
+
+		saynn("Question 1:")
+
+		saynn("Currently our android gender distribution is:")
+
+		saynn("Do you feel comfortable with that?")
+
+		saynn("=============================================")
+
+		saynn("Nano Android Feedback - Inmate Version")
+
+		saynn("=============================================")
+
+		saynn("Question 2:")
+
+		saynn("Currently our android species distribution is:")
+
+		saynn("Do you feel comfortable with that?")
+
+		saynn("Also, if there's a new species entering or leaving BDCC, your record about this part will be erased. Please submit another record after that.")
+
+		saynn("=============================================")
+
+		saynn("Nano Android Feedback - Inmate Version")
+
+		saynn("=============================================")
+
+		saynn("Question 3:")
+
+		saynn("Currently our android body size range is:")
+
+		saynn("Do you feel comfortable with that?")
+
+		saynn("=============================================")
+
+		saynn("Nano Android Feedback - Inmate Version")
+
+		saynn("=============================================")
+
+		saynn("Question 4:")
+
+		saynn("Do you have any suggestions or questions related to this new program? If so, please click the ISSUE button.")
+
+		saynn("(Well, this question is kind of weird. The ISSUE button is a hyperlink connected to the Github issue page of this mod. You can make your suggestions there ^V^)")
+
+		saynn("You complete all these questions. Humoi grab her datapad back and smile.")
+
+		saynn("[say=humoi]Looks like someone is waiting for {pc.hisher} rewards~[/say]")
+
+	if(state == "I don't care about android settings."):
+		saynn("[say=pc]Sorry,I am quite busy now.[/say]")
+
+		saynn("Humoi chuckles and grabs the datapad back.")
+
+		saynn("[say=pc]It's fine. Just remember, you are free to find me and change your options. I'm lilac, so you know you can find me. See you around~[/say]")
+
+		saynn("(end scene)")
+
+	if(state == "Check if the survey is really necessary."):
+		saynn("[say=pc]May I use you without such survey.[/say]")
+
+		saynn("Humoi giggles and grabs the datapad back.")
+
+		saynn("[say=pc]Well, it seems someone is pretty horny today. Yes, use me as an useless sextory~[/say]")
+
+		saynn("(end scene)")
+
 
 func _react(_action: String, _args):
 	if(_action == "endthescene"):
-		setFlag("HypnokinkModule.DidVionGlance", true)
 		endScene()
 		return
 
 	setState(_action)
+
